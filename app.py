eb1affafc2ef70655136087b9fe6d023337c2c6f
Juan Moscoso <juanmos.dev@gmail.com>
2024-06-02 22:11:07 -0500

    terraform

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def health():  # put application's code here
 
 @app.route('/', methods=['GET'])
 def hello_world():  # put application's code here
-    return 'Esta es la API de vide juegossssss!'
+    return 'Esta es la API de vide juegosssss!'
 
 
 if __name__ == '__main__':
